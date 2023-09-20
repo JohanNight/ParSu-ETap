843cbab626ac3bbaddd15e619a316f2a5fc1b545
suelilajohnanthony@gmail.com <suelilajohnanthony@gmail.com>
2023-09-20 13:29:25 +0800

    update

--- resources/views/AdminSide/login.blade.php
+++ resources/views/AdminSide/login.blade.php
@@ -16,35 +16,36 @@
         </div>
     </div>
 </div>
-<section class="flex justify-center items-center">
+<section class="flex justify-around items-center mt-10">
     <header class="m-5 p-2.5 flex items-center">
         <h1 class="text-6xl font-bold text-blue-500">
             ParSU e-Tap Admin
         </h1>
     </header>
-    <div class="bg-gray-100 max-w-lg mx-auto p-8 my-10 rounded-lg shadow-2xl ">
+    <div class="bg-gray-100 max-w-full mx-auto my-10 p-10 rounded-lg shadow-2xl border-2 border-blue-700 ">
         <section class="bg-gray-100">
             <h3 class="font-bold text-2xl">Welcome to ParSU e-TAP Systems</h3>
             <p class="text-gray-600 pt-2">Sign in to your Account</p>
         </section>
+        <div class="mt-2 bg-blue-700 w-full p-2"></div>
         <section class="mt-10">
             <form action="/login/authenticate" method="POST" class="flex flex-col">
                 @csrf
-                <div class="mb-6 pt-3rounded bg-gray-200">
+                <div class="mb-6 pt-3rounded bg-gray-200 rounded-md">
                     <label for="email" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Email</label>
                     <input type="text" name="email" id="email"
                         class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400  px-3"
                         autocomplete="off">
                 </div>
 
-                <div class="mb-6 pt-3rounded bg-gray-200">
+                <div class="mb-6 pt-3rounded bg-gray-200 rounded-md">
                     <label for="password" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Password</label>
                     <input type="password" name="password"
                         class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 borderr-50 px-3">
                 </div>
 
                 <button
-                    class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
+                    class="bg-green-600 w-full hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
                     type="submit">Sign in
                 </button>
 
