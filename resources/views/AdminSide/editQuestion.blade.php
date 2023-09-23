56e1a1a719314108e3f1df9bafeefbaaa896285b
suelilajohnanthony@gmail.com <suelilajohnanthony@gmail.com>
2023-09-23 14:23:07 +0800

    Update: editQuestion Page updated

--- resources/views/AdminSide/editQuestion.blade.php
+++ resources/views/AdminSide/editQuestion.blade.php
@@ -13,41 +13,56 @@
 
             {{-- Survey Number 1 --}}
             <div class="p-3 border-2 border-black w-full">
-                <div class="w-full flex justify-between">
-                    <div>
-                        <h1 class="text-[20px] Bold-font">
-                            Citizen's Charter Question
-                        </h1>
-                    </div>
-                    <div>
-                        <button id="addQuestion_id"
-                            class="text-[15px] SemiB-font text-white bg-green-500 rounded-md p-2">
-                            Add New Question
-                        </button>
-                    </div>
-                </div>
                 <div class="bg-white p-2 w-full shadow-lg mt-4 rounded-md mb-3">
-                    <div class="w-full flex flex-col">
-                        <div class="flex items-center gap-2">
-                            <h1 class="text-[18px] Bold-font">
-                                CC1
+                    <div class="w-full flex justify-between items-center mb-3">
+                        <div>
+                            <h1 class="text-[20px] Bold-font">
+                                Citizen's Charter Question
                             </h1>
-                            <h3 class="text-[15px] Reg-font">
-                                Which of the following best describes your awareness of a CC?
-                            </h3>
-                            <span class=" italic text-sm">
-                                (Former Question)
-                            </span>
+                        </div>
+                        <div>
+                            <button id="addQuestion_id"
+                                class="text-[15px] SemiB-font text-white bg-green-500 rounded-md p-2" type="button">
+                                Add New Question
+                            </button>
+                        </div>
+                    </div>
+                    <div class="w-full flex flex-col border-2 border-black p-2">
+                        <div class="w-full flex justify-end">
+                            <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md" type="button">
+                                Delete
+                            </button>
+                        </div>
+                        <div class="flex flex-col  gap-2">
+                            <div class="flex items-center gap-2">
+                                <h1 class="text-[18px] Bold-font">
+                                    CC1
+                                </h1>
+                                <h3 class="text-[15px] Reg-font">
+                                    Which of the following best describes your awareness of a CC?
+                                </h3>
+                                <span class=" SemiB-font text-sm">
+                                    (Sample Format)
+                                </span>
+                            </div>
+
+                            <div class="flex flex-col text-xs Reg-font ml-2">
+                                <span>1. I know what a CC is and I saw this office’s CC</span>
+                                <span>2. I know what a CC is but I did NOT see this office’s CC.</span>
+                                <span>3. I learned of the CC only when I saw this office’s CC.</span>
+                                <span>4. I do not know what a CC is and I did not see one in this office. (Answer ‘N/A’
+                                    on CC2 and CC3)</span>
+                            </div>
                         </div>
                         <div class="mt-2 mb-3 flex flex-col gap-4">
                             <div class="gap-2">
-                                <label for="question_num" class="text-[12px] Reg-font">Question Number: </label>
+                                <label for="question_num" class="text-[15px] Reg-font">Question Number: </label>
                                 <input type="text" name="question_num" id="question_num"
                                     class="w-20 bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm"
                                     autocomplete="off">
                             </div>
                             <div class="gap-2 flex flex-col">
-                                <label for="question_num" class="text-[12px] Reg-font">Description: </label>
+                                <label for="question_num" class="text-[15px] Reg-font">Description: </label>
                                 {{-- <input type="text" name="question_num" id="question_num"
                                     class="w-96 bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm"
                                     autocomplete="off"> --}}
@@ -74,7 +89,8 @@
                                     <label for="">1.</label>
                                     <input type="text" name="" id=""
                                         class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
-                                    <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                    <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md"
+                                        type="button">
                                         Delete
                                     </button>
                                 </div>
@@ -82,7 +98,8 @@
                                     <label for="">2.</label>
                                     <input type="text" name="" id=""
                                         class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
-                                    <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                    <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md"
+                                        type="button">
                                         Delete
                                     </button>
                                 </div>
@@ -90,7 +107,8 @@
                                     <label for="">3.</label>
                                     <input type="text" name="" id=""
                                         class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
-                                    <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                    <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md"
+                                        type="button">
                                         Delete
                                     </button>
                                 </div>
@@ -98,19 +116,105 @@
                                     <label for="">4.</label>
                                     <input type="text" name="" id=""
                                         class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
-                                    <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                    <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md"
+                                        type="button">
                                         Delete
                                     </button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div>
-
+                </div>
+                <div class="bg-white p-2 w-full shadow-lg mt-4 rounded-md mb-3">
+                    <div class="w-full flex justify-between items-center mb-3">
+                        <div>
+                            <h1 class="text-[20px] Bold-font">
+                                Survey Question
+                            </h1>
+                        </div>
+                        <div>
+                            <button id="addQuestion_id"
+                                class="text-[15px] SemiB-font text-white bg-green-500 rounded-md p-2">
+                                Add New Question
+                            </button>
+                        </div>
+                    </div>
+                    <div class="flex items-center gap-2 mt-2 mb-3">
+                        <h3 class="text-[15px] Reg-font">
+                            SQD0. I am satisfied with the service that I availed.
+                        </h3>
+                        <span class=" SemiB-font text-sm">
+                            (Sample Question)
+                        </span>
+                    </div>
+                    <div class="flex flex-col p-3 w-full gap-4 mt-4">
+                        <div class="flex gap-2 w-full ">
+                            <label for="">1.</label>
+                            <input type="text" name="" id=""
+                                class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
+                            <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                Delete
+                            </button>
+                        </div>
+                        <div class="flex gap-2 w-full ">
+                            <label for="">2.</label>
+                            <input type="text" name="" id=""
+                                class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
+                            <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                Delete
+                            </button>
+                        </div>
+                        <div class="flex gap-2 w-full ">
+                            <label for="">3.</label>
+                            <input type="text" name="" id=""
+                                class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
+                            <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                Delete
+                            </button>
+                        </div>
+                        <div class="flex gap-2 w-full ">
+                            <label for="">4.</label>
+                            <input type="text" name="" id=""
+                                class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
+                            <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                Delete
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <div>
+                <div class="bg-white p-2 w-full shadow-lg mt-4 rounded-md mb-3">
+                    <div class="w-full flex justify-between items-center mb-3">
+                        <div>
+                            <h1 class="text-[20px] Bold-font">
+                                Comment Question
+                            </h1>
+                        </div>
+                        <div>
+                            <button id="addQuestion_id"
+                                class="text-[15px] SemiB-font text-white bg-green-500 rounded-md p-2">
+                                Add New Question
+                            </button>
+                        </div>
 
+                    </div>
+                    <div class="flex flex-col p-3 w-full gap-4 mt-4">
+                        <div class="flex items-center gap-2">
+                            <h3 class="text-[15px] Reg-font capotalize">
+                                Any Comments?
+                            </h3>
+                            <span class="SemiB-font text-sm">
+                                (Sample Question)
+                            </span>
+                        </div>
+                        <div class="flex gap-2 w-full ">
+                            <label for="">1.</label>
+                            <input type="text" name="" id=""
+                                class="w-full bg-gray-100 border-2 px-1 py-0.5 focus:outline-none text-sm tracking-wide">
+                            <button class="bg-red-500 text-white text-sm SemiB-font p-1 rounded-md">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
                 </div>
 
                 {{-- <div class="p-2 border-b-2 border-b-gray-700">
@@ -693,7 +797,7 @@
             {{-- Submit button --}}
             <div class="mt-2 p-10 flex justify-center ">
                 <button class="p-2 text-xl Reg-font bg-green-400 rounded active:bg-green-600" type="button">
-                    Update
+                    Save
                 </button>
             </div>
         </form>
@@ -701,60 +805,5 @@
     </div>
 </div>
 
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        //to retrive the data and the populate them in each fields
-        $(document).ready(function() {
-            var categoryMapping = {
-                'Student': 1,
-                'Faculty': 2,
-                'Personnel': 3,
-                'Others': 4
-            };
-            $('#searchButton').on('click', function() {
-                const csrfToken = $('meta[name="csrf-token"]').attr(
-                    'content'); // Get the CSRF token from the meta tag
-                const searchTerm = $('#search-id').val();
-                // Send AJAX request to the server to fetch client data
-                $.ajax({
-                    url: 'http://127.0.0.1:8000/home/clientSurvey/Search',
-                    method: 'POST',
-                    data: {
-                        '_token': csrfToken,
-                        'searchId': searchTerm
-                    },
-                    dataType: 'json',
-                    success: function(data) {
-                        // Populate client info fields with retrieved data 
-                        setTimeout(() => {
-                            $('#name_of_client').val(data.name);
-                            $('#gender_of_client').val(data.gender);
-                            $('#age_of_client').val(data.age);
-                            // Map the category string to its numeric value using categoryMapping
-                            var numericCategory = categoryMapping[data
-                                .category];
-                            // Set the selected option in the client_type select element based on the numeric value
-                            $('#client_type').val(numericCategory);
-
-                            // Populate other fields as needed
-                        }, 0);
-                    },
-                    error: function(xhr, status, error) {
-                        console.error(error);
-                        console.log("Status:", status);
-                        console.error("XHR Error:", xhr);
-                    }
-                });
-            });
-        });
-
-        // date_autofill.js
-        // Get the current date in the format "YYYY-MM-DD"
-        var today = new Date().toISOString().split('T')[0];
-
-        // Set the input field's value to today's date
-        document.getElementById("date_of_transaction").value = today;
-    });
-</script>
 
 @include('partials.footerClient')
