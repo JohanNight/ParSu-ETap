3be707a314a958cf9afa80f5f12b1152c8aa5cc8
suelilajohnanthony@gmail.com <suelilajohnanthony@gmail.com>
2023-10-29 14:09:59 +0800

    update

--- resources/views/pdf/totalResult.blade.php
+++ resources/views/pdf/totalResult.blade.php
@@ -1,4 +1,74 @@
-@include('partials.headerAdmin')
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        #customers {
+            font-family: Arial, Helvetica, sans-serif, 'poppins';
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #customers td,
+        #customers th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #customers tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #customers tr:hover {
+            background-color: #ddd;
+        }
+
+        #customers th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #1904d6;
+            color: white;
+        }
+
+        #customers td {
+            font-size: 12px;
+        }
+
+        h1 {
+            font-size: 20px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1>Overall Services Surveyed
+        by the
+        University</h1>
+
+    <table id="customers">
+        <tr>
+            <th>Overall Services</th>
+            <th>Responses</th>
+            <th>Total of Transaction</th>
+        </tr>
+        @foreach ($getTotalServiceAvail as $serviceTitle => $count)
+            <tr>
+                <td> {{ $serviceTitle }}</td>
+                <td>{{ $count }}</td>
+                <td>{{ $count }}</td>
+            </tr>
+        @endforeach
+    </table>
+
+</body>
+
+</html>
+
+
+
+{{-- @include('partials.headerAdmin')
 <!-- Table of Survey Service External-->
 <div class="md:w-full bg-white p-3 mt-2 mb-3 shadow-md rounded-lg ">
     <div class="w-full p-2">
@@ -37,4 +107,4 @@
         </tbody>
     </table>
 </div>
-@include('partials.footerAdmin')
+@include('partials.footerAdmin') --}}
