2931a57d7b2c56a67ff21a628ce9e61b719f3dbd
suelilajohnanthony@gmail.com <suelilajohnanthony@gmail.com>
2023-11-28 20:15:13 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,6 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'bg-ParSU': "url('images/PARSU IMAGES/parsuEntrance.jpg')",
-        // 'footer-texture': "url('/img/footer-texture.png')",
-      },
-      safelist: ['animate-[fade-in_1s_ease-in-out]', 'animate-[fade-in-down_1s_ease-in-out]'],
-    },
   },
   plugins: [],
 }
