272eb1fc722e4694d53ee052319611fd412b1662
suelilajohnanthony@gmail.com <suelilajohnanthony@gmail.com>
2023-12-07 16:57:42 +0800

    update

--- app/Http/Controllers/clientController.php
+++ app/Http/Controllers/clientController.php
@@ -29,11 +29,6 @@ class clientController extends Controller
 
     public function showWelcomePage()
     {
-        // if (View::exists('ClientSide.index')) {
-        //     return view('ClientSide.index');
-        // } else {
-        //     return abort(404);
-        // }
         if (View::exists('ClientSide.index')) {
             return view('ClientSide.index');
         } else {
